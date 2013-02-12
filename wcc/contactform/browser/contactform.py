ee99d75778b5e060f4b61e10ca42e13ab0ad4c0d
kulll <anwarbaik88@gmail.com>
2013-02-12 11:50:32 +0800

    it tot its works haizzz

--- wcc/contactform/browser/contactform.py
+++ wcc/contactform/browser/contactform.py
@@ -3,9 +3,8 @@ from zope import interface
 from Products.CMFCore.utils import getToolByName
 from smtplib import SMTPException
 
-
-
 grok.templatedir('templates')
+
 
 class Index(grok.View):
     grok.require('zope2.View')
@@ -13,23 +12,44 @@ class Index(grok.View):
     grok.name('contact-wcc')
     grok.template('contact')
 
-
     def update(self):
         data = self.request.form
+        import ipdb; ipdb.set_trace()
+        if data:
+            self.create_email(data)
+            self.send_email()
 
     def send_email(self):
-        subject = "just a freakin subject"
-        message = "i sent some shit"
         mail_host = getToolByName(self.context, 'MailHost')
 
         # Send email
         try:
             mail_host.send(
-                    message,
-                    "no-reply@oikoumene.org",
-                    "no-reply@oikoumene.org",
-                    subject,
+                    self.message,
+                    "anwarorchid88@gmail.com",
+                    "anwarorchid88@gmail.com",
+                    self.subject,
                     charset='utf-8'
                     )
         except SMTPException:
             raise SMTPException('Recipient address rejected by server')
+
+    def create_email(self, data):
+        email = data['email']
+        name = data['name']
+        city = data['city']
+        country = data['country']
+        phone = data['phone']
+        original_message = data['message']
+        self.subject = data['subject']
+
+        self.message = """%s
+
+
+        %s
+        %s
+        %s
+        %s
+        %s
+
+        """ % (original_message, email, name, city, country, phone)
